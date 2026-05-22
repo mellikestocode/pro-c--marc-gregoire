389635506a5c07b6460197238e053eda99d05446
mellymel <sometotallyfakeemail@fakestemail.com>
2026-05-22 10:21:52 -0400

    added an uninitialized array. Added a helpful comment

--- 01_helloworld.cpp
+++ 01_helloworld.cpp
@@ -1,24 +1,25 @@
 import std;
-import std::numeric_limits;
+
+using namespace std;
 
 int main()
 {
-    std::println("Hello, World!");
+    println("Hello, World!");
     int value;
-    std::cin >> value;
-    std::println("You entered {}", value);
+    cin >> value;
+    println("You entered {}", value);
 
-    std::println("int: ");
-    std::println("Max int value: {}", numeric_limits<int>::max());
-    std::println("Min int value: {}", numeric_limits<int>::min());
-    std::println("lowest int value: {}", numeric_limits<int>::lowest());
+    println("int: ");
+    println("Max int value: {}", numeric_limits<int>::max());
+    println("Min int value: {}", numeric_limits<int>::min());
+    println("lowest int value: {}", numeric_limits<int>::lowest());
 
-    std::println("\ndouble: ");
-    std::println("Max double value: {}", numeric_limits<double>::max());
-    std::println("Min double value: {}", numeric_limits<double>::min());
-    std::println("Lowest double value: {}", numeric_limits<double>::lowest());
+    println("\ndouble: ");
+    println("Max double value: {}", numeric_limits<double>::max());
+    println("Min double value: {}", numeric_limits<double>::min());
+    println("Lowest double value: {}", numeric_limits<double>::lowest());
 
-    std::println("\nCasting values instead of joining a casting couch crew");
+    println("\nCasting values instead of joining a casting couch crew");
 
     float myFloat { 3.14f };
     int i1 { (int)myFloat };
@@ -35,6 +36,7 @@ int main()
 
     Employee* anEmployee { new Employee { 'J', 'D', 42, 80'000 } };
     println("{}", (*anEmployee).salary);
+    println("{}", anEmployee->salary);
     delete anEmployee; anEmployee = nullptr;
     return 0; // return non-zero value if error
 }
